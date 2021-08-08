20bf20ed6bfb633d90366c4ab521897a72134937
RoryEvers <roryevers2000@hotmail.com>
2021-08-08 20:05:08 +1000

    Style all pages

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,11 @@
 class Job < ApplicationRecord
   # belongs_to :profile
-  belongs_to :developer, class_name: "Profile", optional: true
+  # belongs_to :developer, class_name: "Profile", optional: true
   belongs_to :creator, class_name: "Profile"
   
+  # has many profiles through current devs
+def current_volunteer
+  
+end
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,6 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :jobs_to_create, class_name: "Job", foreign_key: "creator_id"
   has_many :jobs_to_do, class_name: "Job", foreign_key: "developer_id"
+
+  # has many jobs through current devs
 end
